30def52372a873f73cfb01568478e4511f12efef
user <user@FB01>
2020-03-17 11:36:45 +0800

    change the indentation

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -2,22 +2,22 @@ package firstProject;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
-        String[] lyriceSplit = lyrics.split(" ");
-        
-        lyrics = "";
-        
-        for(String i: lyriceSplit) {
-        	if(i.equals("it")) {
-        		lyrics += "\"her\" ";
-        	} else if(i.equals("Cannot")){
-        		lyrics += "Can't ";
-        	} else {
-        		lyrics += (i + " ");
-        	}
-        }
-        System.out.println(lyrics);
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+    String[] lyriceSplit = lyrics.split(" ");
+
+    lyrics = "";
+
+    for (String i : lyriceSplit) {
+      if (i.equals("it")) {
+        lyrics += "\"her\" ";
+      } else if (i.equals("Cannot")) {
+        lyrics += "Can't ";
+      } else {
+        lyrics += (i + " ");
+      }
+    }
+    System.out.println(lyrics);
+  }
 }
