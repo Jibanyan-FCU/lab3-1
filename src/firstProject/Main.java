3eb6f1b9ba0c6f465d08ac62c3661f966344ed30
user <user@FB01>
2020-03-17 11:29:50 +0800

    Submit Homework

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -4,7 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+        String[] lyriceSplit = lyrics.split(" ");
+        
+        lyrics = "";
+        
+        for(String i: lyriceSplit) {
+        	if(i.equals("it")) {
+        		lyrics += "\"her\" ";
+        	} else if(i.equals("Cannot")){
+        		lyrics += "Can't ";
+        	} else {
+        		lyrics += (i + " ");
+        	}
+        }
+        System.out.println(lyrics);
 	}
-
 }
